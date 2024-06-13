bdf6ce7d930b49976641007af9f9b4e3348c79fd
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2024-06-12 19:06:08 -0700

    style the styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+	import './styles.css';
+</script>
+
+
+<div class="app">
+    <main>
+        <slot/>
+    </main>
+</div>
+
+
+<style>
+    :global(html, body) {
+        height: 100%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+    .app {
+        --bg-color: rgb(37, 37, 37);
+        --font-color: rgb(230, 230, 230);
+        --accent-light: rgb(66, 66, 66);
+        --accent-hover: rgb(53, 53, 53);
+        --accent-dark: rgb(31, 31, 31);
+        --redirect-color: rgb(29, 93, 233);
+
+        background-color: var(--bg-color);
+        color: var(--font-color);
+
+        min-height: 100vh;
+        height: fit-content;
+        width: 100vw;
+        margin: 0 auto;
+        overflow: hidden;
+        
+    }
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button>I AM A <s>SURGEON</s> BUTTON</button>
+
+<input type="text" default="ligma" placeholder="ligma">
